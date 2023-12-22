cea506a94f9e159c3e5155022f665d1d1442b590
Taylor Brown <78773029+TaylorBrown96@users.noreply.github.com>
2023-12-22 16:30:21 -0500

    Add files via upload

--- Datathon/datathon.py
+++ Datathon/datathon.py
@@ -27,17 +27,17 @@ def get_race(img):
 
 # Gets the names of all the images in the folder and returns them in a list
 def get_image_names(folderName):
-    folder_path = folderName
-    image_names = os.listdir(folder_path)
-    return image_names
-
+    image_names = os.listdir(folderName)
+    image_names = [name for name in image_names if name.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    return image_names    
+    
 # Writes the results for the deepface analysis to a csv file
 def writeCSV(results_dict): 
     # Open the csv file and write the column names to the first row of the file then writes the results to the file in a for loop
-    with open('results.csv', 'a') as f:
+    with open('results.csv', 'w') as f:
         f.write("filename,age,gender,race\n")
         for value in results_dict.items():
-            filename = value[0].split('/')[1]
+            filename = os.path.basename(value[0])
             age = value[1]['age']
             try:
                 gender = max(value[1]['gender'], key=value[1]['gender'].get)
@@ -53,17 +53,18 @@ def writeCSV(results_dict):
 # Main function that calls the other functions to get the results for the images in the folder and write them to a csv file
 def main(folderName='faceimages'):
     images = get_image_names(folderName)
-    
+
     results_dict = {}
     index = 1
     for image in images:
         print("Processing image " + str(index) + "/" + str(len(images)) + ": ", image)
-        image = folderName + '/' + image
+        image = os.path.join(folderName, image)
         age = get_age(image)
         gender = get_gender(image)
         race = get_race(image)
         results_dict[image] = {'age': age, 'gender': gender, 'race': race}
         index += 1
+    print(results_dict)
     
     writeCSV(results_dict)
         
